7b5fbaaf11ca84e94a42e578e99c44d86c73e039
Himanshi <147384570+himanshi-0908@users.noreply.github.com>
2025-07-13 01:16:11 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DecentVoice',
+  description: 'Created by Himanshi',
 }
 
 export default function RootLayout({
